a35982bc8ac60ec736f18f41332136f6d56039f0
Yurim Kim <wendy7kim@gmail.com>
2025-06-01 22:43:23 +0900

    Initial Commit

---
+++ SlotIn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlotIn
+//
+//  Created by Yulim KIm on 6/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlotIn/SlotInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotInApp.swift
+//  SlotIn
+//
+//  Created by Yulim KIm on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SlotInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
